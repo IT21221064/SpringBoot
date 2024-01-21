a46412f2141e284a9d0731ca19e073302e285c25
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-21 23:23:06 +0530

    get all rooms function added

--- client/lakeSide-hotel/src/components/room/AddRoom.jsx
+++ client/lakeSide-hotel/src/components/room/AddRoom.jsx
@@ -51,6 +51,10 @@ const AddRoom = () => {
     } catch (error) {
       seterrorMessage(error.message);
     }
+    setTimeout(() => {
+      setsuccessMessage("");
+      seterrorMessage("");
+    }, 3000);
   };
 
   return (
@@ -59,6 +63,15 @@ const AddRoom = () => {
         <div className="row justify-content-center">
           <div className="col-md-8 col-lg-6">
             <h2 className="mt-5 mb-2">Add a New Room</h2>
+            {successMessage && (
+              <div className="alert alert-success fade show">
+                {successMessage}
+              </div>
+            )}
+            {errorMessage && (
+              <div className="alert alert-danger fade show">{errorMessage}</div>
+            )}
+
             <form onSubmit={handleSubmit}>
               <div className="mb-3">
                 <label htmlFor="roomType" className="form-label">
--- lakeSide-hotel/lakeSide-hotel/src/main/java/com/example/lakeSidehotel/service/IBookedRoomService.java
+++
@@ -1,4 +0,0 @@
-package com.example.lakeSidehotel.service;
-
-public interface IBookedRoomService {
-}
