e4c93901ceb7feb6b7f04f970dc47c11ca24911a
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-22 23:02:09 +0530

    filter2

--- client/lakeSide-hotel/src/components/common/RoomFilter.jsx
+++ client/lakeSide-hotel/src/components/common/RoomFilter.jsx
@@ -2,8 +2,41 @@ import React from "react";
 import { useState } from "react";
 
 const RoomFilter = () => {
-  const [filter, setFilter] = useState();
-  return <div></div>;
+  const [filter, setFilter] = useState("");
+
+  const handleSelectChange = (e) => {
+    const selectedRoomType = e.target.value;
+    setFilter(selectedRoomType);
+    const filteredRooms = data.filter((room) =>
+      room.roomType.toLowerCase().includes(selectedRoomType.toLowerCase())
+    );
+    setFilteredData(filteredRooms);
+  };
+  const clearFilter = () => {
+    setFilter("");
+    setFilteredData(data);
+  };
+
+  const roomTypes = ["", ...new Set(data.map((room) => room.roomType))];
+  return (
+    <div className="input-group mb-3">
+      <span className="input-group-text" id="room-type-filter">
+        Filter rooms by types
+      </span>
+      <select
+        className="form-select"
+        value={filter}
+        onChange={handleSelectChange}
+      >
+        <option value={""}> select a room type to filter ...</option>
+        {roomTypes.map((type, index) => (
+          <option key={index} value={type}>
+            {}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 };
 
 export default RoomFilter;
