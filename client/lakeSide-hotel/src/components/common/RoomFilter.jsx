8e09dfb6af4d16b62ec942de403366f158963307
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-22 21:48:59 +0530

    creating Room Filter

---
+++ client/lakeSide-hotel/src/components/common/RoomFilter.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState } from "react";
+
+const RoomFilter = () => {
+  const [filter, setFilter] = useState();
+  return <div></div>;
+};
+
+export default RoomFilter;
