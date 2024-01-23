12201cce2d83f0be07568ce28489119f3f651395
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-23 14:47:15 +0530

    pt2

--- client/lakeSide-hotel/src/components/room/ExistingRooms.jsx
+++ client/lakeSide-hotel/src/components/room/ExistingRooms.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import { useState } from "react";
+import { getAllRooms } from "../utils/ApiFunctions";
 
 const ExistingRooms = () => {
   const [rooms, setRooms] = useState([]);
-  return <div>ExistingRooms</div>;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [roomsPerPage] = useState(8);
+  const [isLoading, setIsLoading] = useState(false);
+  const [filteredRoomes, setFilteredRoom] = useState([]);
+  const [selectedRoomType, setSelectedRoomTypes] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessag] = useState("");
+
+  useEffect(() => {
+    fetchRooms();
+  }, []);
+
+  const fetchRooms = async () => {
+    setIsLoading(true);
+    try {
+      const result = await getAllRooms();
+      setRooms(result);
+      setIsLoading(false);
+    } catch (error) {
+      setErrorMessag(error);
+    }
+  };
+
+  return <div></div>;
 };
 
 export default ExistingRooms;
