06ad91437c6eec701853fb6294a82fc291954ff8
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-23 14:24:28 +0530

    roomspage-p1

---
+++ client/lakeSide-hotel/src/components/room/ExistingRooms.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState } from "react";
+
+const ExistingRooms = () => {
+  const [rooms, setRooms] = useState([]);
+  return <div>ExistingRooms</div>;
+};
+
+export default ExistingRooms;
