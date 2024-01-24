97a5639f6ee993a5d48db136566e254fb7187e8a
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-24 17:34:07 +0530

    pt4

--- client/lakeSide-hotel/src/components/room/ExistingRooms.jsx
+++ client/lakeSide-hotel/src/components/room/ExistingRooms.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { getAllRooms } from "../utils/ApiFunctions";
+import RoomFilter from "../common/RoomFilter";
+import RoomPaginator from "../common/RoomPaginator";
+import { Col } from "react-bootstrap";
 
 const ExistingRooms = () => {
   const [rooms, setRooms] = useState([]);
@@ -39,6 +42,10 @@ const ExistingRooms = () => {
     setCurrentPage(1);
   }, [rooms, selectedRoomType]);
 
+  const handlePaginationClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   const calculateTotalPages = (filteredRooms, roomsPerPage, rooms) => {
     const totalRooms =
       filteredRooms.length > 0 ? filteredRooms.length : rooms.length;
@@ -59,6 +66,42 @@ const ExistingRooms = () => {
             <div className="d-flex justify-content-center mb-3 mt-5">
               <h2>Existing Rooms</h2>
             </div>
+            <Col md={6} className="mb-3 mb-md-0">
+              <RoomFilter data={rooms} setFilteredData={setFilteredRoom} />
+            </Col>
+
+            <table className="table table-bordered table-hover">
+              <thead>
+                <tr className="text-center">
+                  <th>ID</th>
+                  <th>Room Type</th>
+                  <th>Room Price</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {currentRooms.map((room) => (
+                  <tr key={room.id} className="text-center">
+                    <td>{room.id}</td>
+                    <td>{room.roomType}</td>
+                    <td>{room.roomPrice}</td>
+                    <td>
+                      <button>View / Edit</button>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <RoomPaginator
+              currentPage={currentPage}
+              totalPages={calculateTotalPages(
+                filteredRooms,
+                roomsPerPage,
+                rooms
+              )}
+              onPageChange={handlePaginationClick}
+            />
           </section>
         </>
       )}
