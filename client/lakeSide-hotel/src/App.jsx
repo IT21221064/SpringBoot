92a3a9b189cd9af05470d0d15e038e9ea5d55ae1
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-25 15:06:11 +0530

    routers done

--- client/lakeSide-hotel/src/App.jsx
+++ client/lakeSide-hotel/src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
-import AddRoom from "./components/room/AddRoom";
+import ExistingRooms from "./components/room/ExistingRooms";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import EditRoom from "./components/room/EditRoom";
 
 function App() {
   return (
     <>
-      <AddRoom />
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-rooms" element={<ExistingRooms />} />
+          </Routes>
+        </Router>
+      </main>
     </>
   );
 }
