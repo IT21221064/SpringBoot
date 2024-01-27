63e0fdaa3b9cccaafebdf40fa623713bafef5a72
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-27 23:05:10 +0530

    navbar done half

--- client/lakeSide-hotel/src/components/layout/NavBar.jsx
+++ client/lakeSide-hotel/src/components/layout/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
@@ -9,20 +10,61 @@ const NavBar = () => {
           <span className="hotel-color"> lakeSide Hotel</span>
         </Link>
         <button
-          className="navbartoggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarScroll"
-          aria-aria-controls="#navbarScroll"
+          aria-aria-controls="navbarScroll"
           arial-expanded="false"
-          aria-label="Toggle-navigation"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarScroll">
           <ul className="navbar-nav me-auto my-2 my-lg-0 navabar-nav-scroll">
             <li className="nav-item">
-              <NavLink>Browse all rooms</NavLink>
+              <NavLink
+                className="nav-link"
+                aria-current="page"
+                to={"/brows-all-rooms"}
+              >
+                Browse all rooms
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to={"/admin"}>
+                Admin
+              </NavLink>
+            </li>
+          </ul>
+
+          <ul className="d-flex navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" to={"/find-booking"}>
+                Find My Booking
+              </NavLink>
+            </li>
+            <li className="nav-item dropdown">
+              <a></a>
+              <ul>
+                <li>
+                  <Link to={"/login"} className="dropdown-item">
+                    Login
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to={"/profile"} className="dropdown-item">
+                    Profile
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to={"/logout"} className="dropdown-item">
+                    Logout
+                  </Link>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
