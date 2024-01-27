09c37ef807577736198a299fcf8b7f7930148320
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-27 23:05:19 +0530

    footer done

--- client/lakeSide-hotel/src/components/layout/Footer.jsx
+++ client/lakeSide-hotel/src/components/layout/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   let today = new Date();
 
   return (
-    <footer className="by-dark text-light py-3 footer mt-lg-5">
+    <footer className="bg-dark text-light py-3 footer mt-lg-5">
       <Container>
         <Row>
           <Col xs={12} md={12} className="text-center">
