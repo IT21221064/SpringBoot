cd6f54e14de5d3f0b30eac07d5ed0509c210b01c
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-27 20:39:05 +0530

    adding pages

--- client/lakeSide-hotel/src/App.jsx
+++ client/lakeSide-hotel/src/App.jsx
@@ -5,6 +5,7 @@ import ExistingRooms from "./components/room/ExistingRooms";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import EditRoom from "./components/room/EditRoom";
+import AddRoom from "./components/room/AddRoom";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
             <Route path="/existing-rooms" element={<ExistingRooms />} />
+            <Route path="/add-room" element={<AddRoom />} />
           </Routes>
         </Router>
       </main>
