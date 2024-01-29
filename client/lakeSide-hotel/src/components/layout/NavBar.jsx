ebf04904469203c120650673a1a0e505131f669f
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-29 22:16:30 +0530

    Navbar changed

--- client/lakeSide-hotel/src/components/layout/NavBar.jsx
+++ client/lakeSide-hotel/src/components/layout/NavBar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { NavLink } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   const [showAccount, setShowAccount] = useState(false);
