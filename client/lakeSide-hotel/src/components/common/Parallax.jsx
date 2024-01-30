acfdf4b3cc333b5ed900b23a04f63d364607f308
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-30 22:24:33 +0530

    parallex done

--- client/lakeSide-hotel/src/components/common/Parallax.jsx
+++ client/lakeSide-hotel/src/components/common/Parallax.jsx
@@ -4,11 +4,12 @@ import { Container } from "react-bootstrap";
 const Parallax = () => {
   return (
     <div className="parallax mb-5">
-      <Container>
-        <div>
+      <Container className="text-center px-5 py-5 justify-content-center">
+        <div className="animated-texts bounceIn">
           <h1>
             Welcome to <span className="hotel-color">lakeSide Hotel</span>
           </h1>
+          <h3>We offer the best services for all your need</h3>
         </div>
       </Container>
     </div>
