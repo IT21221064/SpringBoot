768b1b921344eed9c413f5b867fa06ea3907b964
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-30 22:32:19 +0530

    Hotel service p2 done

--- client/lakeSide-hotel/src/components/common/HotelService.jsx
+++ client/lakeSide-hotel/src/components/common/HotelService.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import Header from "./Header";
+import { FaClock } from "react-icons/fa";
 
 const HotelService = () => {
   return (
     <Container>
       <Header title={"Our Services"} />
+      <Row>
+        <h4 className="text-center">
+          Services at <span className="hotel-color">lakeSide - </span>Hotel
+          <span className="gap-2">
+            <FaClock /> - 24-Hour Front Desk
+          </span>
+        </h4>
+      </Row>
+      <hr />
+      <Row></Row>
     </Container>
   );
 };
