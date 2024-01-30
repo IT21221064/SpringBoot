d87d4a61d156a9cad5e5681770c277319add23c0
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-30 22:08:10 +0530

    Parallax done

---
+++ client/lakeSide-hotel/src/components/common/Parallax.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Parallax = () => {
+  return (
+    <div className="parallax mb-5">
+      <Container>
+        <div>
+          <h1>
+            Welcome to <span className="hotel-color">lakeSide Hotel</span>
+          </h1>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Parallax;
