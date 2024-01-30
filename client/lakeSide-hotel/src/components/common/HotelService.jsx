0ddca5dd33964f5d0f9c3dde8f0f7a3587ddfeea
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-30 22:24:56 +0530

    Services component done p1

---
+++ client/lakeSide-hotel/src/components/common/HotelService.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./Header";
+
+const HotelService = () => {
+  return (
+    <Container>
+      <Header title={"Our Services"} />
+    </Container>
+  );
+};
+
+export default HotelService;
