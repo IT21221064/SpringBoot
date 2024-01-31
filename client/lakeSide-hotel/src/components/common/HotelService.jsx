f738bede1583e5caa789bd5f7a32cb3eb11a14df
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-31 21:43:28 +0530

    Services pages done

--- client/lakeSide-hotel/src/components/common/HotelService.jsx
+++ client/lakeSide-hotel/src/components/common/HotelService.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import Header from "./Header";
-import { FaClock } from "react-icons/fa";
+import {
+  FaClock,
+  FaCocktail,
+  FaParking,
+  FaTshirt,
+  FaUtensils,
+  FaWifi,
+} from "react-icons/fa";
 
 const HotelService = () => {
   return (
@@ -16,7 +23,91 @@ const HotelService = () => {
         </h4>
       </Row>
       <hr />
-      <Row></Row>
+      <Row xs={1} md={2} lg={3} className="g-4 mt-2">
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title className="hotel-color">
+                <FaWifi />
+                Wifi
+              </Card.Title>
+              <Card.Text>
+                Stay connected with high-speed internet access.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title className="hotel-color">
+                <FaUtensils />
+                Breakfast
+              </Card.Title>
+              <Card.Text>
+                Start your day with a delicious breakfast buffet.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title className="hotel-color">
+                <FaTshirt />
+                Laundry
+              </Card.Title>
+              <Card.Text>
+                Keep your clothes clean and fresh with our laundry service.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title className="hotel-color">
+                <FaUtensils />
+                Breakfast
+              </Card.Title>
+              <Card.Text>
+                Start your day with a delicious breakfast buffet.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title className="hotel-color">
+                <FaCocktail />
+                Mini-bar
+              </Card.Title>
+              <Card.Text>
+                Enjoy a refreshing drink or snack from our in-room mini-bar
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title className="hotel-color">
+                <FaParking />
+                Parking
+              </Card.Title>
+              <Card.Text>
+                Stay cool and comfortable with our air conditioning system.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 };
