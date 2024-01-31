2e7596241f753bca8fef703087d8ab6c9393c135
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-31 22:03:27 +0530

    homepage done p2

--- client/lakeSide-hotel/src/components/home/Home.jsx
+++ client/lakeSide-hotel/src/components/home/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import HotelService from "../common/HotelService";
+import HeaderMain from "../layout/HeaderMain";
+import Parallax from "../common/Parallax";
 
 const Home = () => {
-  return <div>Welcome to the home page</div>;
+  return (
+    <section>
+      <HeaderMain />
+
+      <section className="container">
+        <Parallax />
+        <HotelService />
+        <Parallax />
+      </section>
+    </section>
+  );
 };
 
 export default Home;
