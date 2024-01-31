de1a652934687cfc687ad481b9a12b2b0c51d8f4
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-01-31 22:25:07 +0530

    image slider p2

--- client/lakeSide-hotel/src/components/common/RoomCarousel.jsx
+++ client/lakeSide-hotel/src/components/common/RoomCarousel.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
+import { getAllRooms } from "../utils/ApiFunctions";
 
 const RoomCarousel = () => {
   const [rooms, setRooms] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getAllRooms()
+      .then((data) => {
+        setRooms(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+        setIsLoading(false);
+      });
+  }, []);
 
   return <div></div>;
 };
