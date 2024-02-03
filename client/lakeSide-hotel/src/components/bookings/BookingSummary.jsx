12ccc042a12fea14c571acb6553a6150c5c68aa4
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-02-03 09:22:25 +0530

    Booking summary done

--- client/lakeSide-hotel/src/components/bookings/BookingSummary.jsx
+++ client/lakeSide-hotel/src/components/bookings/BookingSummary.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 const BookingSummary = ({ booking, payment, isFormValid, onConfirm }) => {
   const checkInDate = moment(booking.checkInDate);
   const checkOutDate = moment(booking.checkOutDate);
-  const numOfDays = checkOutDate.diff(checkInDate, "days");
+  const numberOfDays = checkOutDate.diff(checkInDate, "days");
   const [isBookingConfirmed, setIsBookingConfirmed] = useState(false);
   const [isProcessingPayment, setIsProcessingPayment] = useState(false);
   const navigate = useNavigate();
@@ -38,15 +38,15 @@ const BookingSummary = ({ booking, payment, isFormValid, onConfirm }) => {
       </p>
       <p>
         Check-In Date :{" "}
-        <strong>{moment(booking.checkInDate).format("MM DD YYYY")}</strong>
+        <strong>{moment(booking.checkInDate).format("MMM Do YYYY")}</strong>
       </p>
       <p>
         Check-Out Date :{" "}
-        <strong>{moment(booking.checkOutDate).format("MM DD YYYY")}</strong>
+        <strong>{moment(booking.checkOutDate).format("MMM Do YYYY")}</strong>
       </p>
       <p>
         Number of Days:
-        <strong>{numOfDays}</strong>
+        <strong>{numberOfDays}</strong>
       </p>
       <div>
         <h5>Number of Guests</h5>
@@ -56,6 +56,41 @@ const BookingSummary = ({ booking, payment, isFormValid, onConfirm }) => {
         </strong>
         <strong>Children : {booking.numberOfChildren}</strong>
       </div>
+      {payment > 0 ? (
+        <>
+          <p>
+            Total Payment : <strong>${payment}</strong>
+          </p>
+
+          {isFormValid && !isBookingConfirmed ? (
+            <Button variant="success" onClick={handleConfirmBooking}>
+              {isProcessingPayment ? (
+                <>
+                  <span
+                    className="spinner-border spinner-border-sm mr-2"
+                    role="status"
+                    aria-hidden="true"
+                  ></span>
+                  Booking Confirmed, redirecting to payment ....
+                </>
+              ) : (
+                "Confirm Booking and proceed to payment"
+              )}
+            </Button>
+          ) : isBookingConfirmed ? (
+            <div className="d-flex justify-content-center align-items-center">
+              <div className="spinner-border text-primary" role="status">
+                <span className="sry-only">Loading</span>
+              </div>
+            </div>
+          ) : null}
+        </>
+      ) : (
+        <p className="text-danger">
+          Check-out date must be after check-in date.
+        </p>
+      )}
+      ;
     </div>
   );
 };
