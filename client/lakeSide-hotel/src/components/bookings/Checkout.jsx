222d75bac69b5daf0c54d913772bdd818b228711
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-02-05 22:11:51 +0530

    room info adding to the checkout page p1

--- client/lakeSide-hotel/src/components/bookings/Checkout.jsx
+++ client/lakeSide-hotel/src/components/bookings/Checkout.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { getRoomById } from "../utils/ApiFunctions";
 import BookingForm from "./BookingForm";
 
 const Checkout = () => {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [roomInfo, setRoomInfo] = useState({
+    photo: "",
+    roomType: "",
+    roomPrice: "",
+  });
+  const { roomId } = useParams();
+
+  useEffect(() => {
+    setTimeout(() => {
+      getRoomById(roomId)
+        .then((response) => {
+          setRoomInfo(response);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          setError(error);
+          setIsLoading(false);
+        });
+    }, 2000);
+  }, [roomId]);
+
   return (
     <div>
       <BookingForm />
