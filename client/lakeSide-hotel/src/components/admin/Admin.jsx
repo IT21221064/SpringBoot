39b7cac7504a358fc2520ef6a34649e63b2b0032
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-02-07 23:59:19 +0530

    change admin page

--- client/lakeSide-hotel/src/components/admin/Admin.jsx
+++ client/lakeSide-hotel/src/components/admin/Admin.jsx
@@ -6,7 +6,9 @@ const Admin = () => {
     <section className="container mt-5">
       <h2>Welcome to Admin Panel</h2>
       <hr />
-      <Link to={"/add-room"}>Manage Rooms</Link>
+      <Link to={"/existing-rooms"}>Manage Rooms</Link>
+      <br />
+      <Link to={"/existing-bookings"}>Manage Bookings</Link>
     </section>
   );
 };
