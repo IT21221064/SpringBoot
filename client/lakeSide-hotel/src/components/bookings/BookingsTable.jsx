77d15d6eab92039e1311deb137553fcada294977
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-02-07 23:22:30 +0530

    bookings table page done

--- client/lakeSide-hotel/src/components/bookings/BookingsTable.jsx
+++ client/lakeSide-hotel/src/components/bookings/BookingsTable.jsx
@@ -26,7 +26,60 @@ const BookingsTable = ({ bookingInfo, handleBookingCancellation }) => {
     setFilteredBookings(bookingInfo);
   }, [bookingInfo]);
 
-  return <div>BookingsTable</div>;
+  return (
+    <section className="p-4">
+      <DateSlider
+        onDateChange={filterBookings}
+        onFilterChange={filterBookings}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>S/N</th>
+            <th>Booking Id</th>
+            <th>Room Id</th>
+            <th>Check-In Date</th>
+            <th>Check-Out Date</th>
+            <th>Guest Name</th>
+            <th>Guest Email</th>
+            <th>Adults</th>
+            <th>Children</th>
+            <th>Totlal Guest</th>
+            <th>Confirmation Code</th>
+            <th colSpan={2}>Actions</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          {filterBookings.map((booking, index) => (
+            <tr key={booking.id}>
+              <td>{index + 1}</td>
+              <td>{booking.id}</td>
+              <td>{booking.room.id}</td>
+              <td>{booking.checkInDate}</td>
+              <td>{booking.checkOutDate}</td>
+              <td>{booking.guestName}</td>
+              <td>{booking.guestEmail}</td>
+              <td>{booking.numOfAdults}</td>
+              <td>{booking.numOfChildren}</td>
+              <td>{booking.totalNumOfGuests}</td>
+              <td>{booking.bookingConfirmationCode}</td>
+              <td>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => handleBookingCancellation(booking.id)}
+                >
+                  Cancel
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {filterBookings.length === 0 && (
+        <p>No booking found for the selected dates</p>
+      )}
+    </section>
+  );
 };
 
 export default BookingsTable;
