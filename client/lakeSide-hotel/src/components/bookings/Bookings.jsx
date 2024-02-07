7cda4a54beedb7105708484250804e27d76ffc83
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-02-07 23:23:11 +0530

    functions of bookings page done

---
+++ client/lakeSide-hotel/src/components/bookings/Bookings.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { cancelBooking, getAllBookings } from "../utils/ApiFunctions";
+
+const Bookings = () => {
+  const [bookingInfo, setBookingInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      getAllBookings()
+        .then((data) => {
+          setBookingInfo(data);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          setError(error.message);
+          setIsLoading(false);
+        });
+    }, 1000);
+  });
+
+  const handleBookingCancellation = async (bookingId) => {
+    try {
+      await cancelBooking(bookingId);
+      const data = await getAllBookings();
+      setBookingInfo(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return <></>;
+};
+
+export default Bookings;
