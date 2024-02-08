f7f089ad6af4d91822f312364972290eb70e5ed9
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-02-08 22:34:07 +0530

    find booking function p2 done

--- client/lakeSide-hotel/src/components/bookings/FindBooking.jsx
+++ client/lakeSide-hotel/src/components/bookings/FindBooking.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { getBookingByConfirmationCode } from "../utils/ApiFunctions";
 
 const FindBooking = () => {
   const [confirmationCode, setConfirmationCode] = useState("");
@@ -19,7 +20,7 @@ const FindBooking = () => {
   });
   const [isDeleted, setDeleted] = useState(false);
   const clearBookingInfo = {
-    id: "",
+    bookingId: "",
     room: { id: "" },
     checkInDate: "",
     checkOutDate: "",
@@ -29,6 +30,27 @@ const FindBooking = () => {
     numOfChildren: "",
     totalNumOfGuest: "",
     bookingConfirmationCode: "",
+  };
+  const handleInputChange = (e) => {
+    setConfirmationCode(e.target.value);
+  };
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const data = await getBookingByConfirmationCode(confirmationCode);
+      setBookingInfo(data);
+    } catch (error) {
+      setBookingInfo(clearBookingInfo);
+      if (error.response && error.response.status === 404) {
+        setError(error.response.data.message);
+      } else {
+        setError(error.response);
+      }
+    }
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
   };
   return <div>FindBooking</div>;
 };
