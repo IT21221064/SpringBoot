7134f9931eb55699a0addfa0fdb767619ad67c42
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-02-14 22:33:30 +0530

    search result component done

--- client/lakeSide-hotel/src/components/common/RoomSearchResult.jsx
+++ client/lakeSide-hotel/src/components/common/RoomSearchResult.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useState } from "react";
+import { Row, Button } from "react-bootstrap";
+import RoomCard from "../room/RoomCard";
+import RoomPaginator from "../common/RoomPaginator";
 
 const RoomSearchResult = ({ results, onClearSearch }) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -15,7 +18,34 @@ const RoomSearchResult = ({ results, onClearSearch }) => {
   const endIndex = startIndex + resultPerPage;
   const paginatedResult = results.slice(startIndex, endIndex);
 
-  return <div>RoomSearchResult</div>;
+  return (
+    <>
+      {results.length > 0 ? (
+        <>
+          <h5 className="text-center mt-5">Search Result</h5>
+          <Row>
+            {paginatedResult.map((room) => (
+              <RoomCard key={room.id} room={room} />
+            ))}
+          </Row>
+          <Row>
+            {totalResults > resultPerPage && (
+              <RoomPaginator
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChaneg={handlePageChange}
+              />
+            )}
+            <Button variant="secondary" onClick={onClearSearch}>
+              Clear Search
+            </Button>
+          </Row>
+        </>
+      ) : (
+        <p></p>
+      )}
+    </>
+  );
 };
 
 export default RoomSearchResult;
