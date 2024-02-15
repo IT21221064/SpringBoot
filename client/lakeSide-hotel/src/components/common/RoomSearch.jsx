9fa5c6576195f80920cf85fdb46bbedd38e09ca6
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-02-15 12:36:51 +0530

    room search funtions done

---
+++ client/lakeSide-hotel/src/components/common/RoomSearch.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import moment from "moment";
+import { getAvailableRooms } from "../utils/ApiFunctions";
+
+const RoomSearch = () => {
+  const [searchQuery, setSearchQuery] = useState({
+    checkInDate: "",
+    checkOutDate: "",
+    roomType: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [availableRooms, setAvailableRooms] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const checkIn = moment(searchQuery.checkInDate);
+    const checkOut = moment(searchQuery.checkOutDate);
+    if (!checkIn.isValid() || !checkOut.inValid()) {
+      setErrorMessage("Please, enter valid date range");
+      return;
+    }
+    if (!checkOut.isSameOrAfter(checkIn)) {
+      setErrorMessage("Check-In date must come before Check-Out date");
+      return;
+    }
+    setIsLoading(true);
+    getAvailableRooms(
+      searchQuery.checkInDate,
+      searchQuery.checkOutDate,
+      searchQuery.roomType
+    )
+      .then((response) => {
+        setAvailableRooms(response.data);
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    const checkIn = moment(searchQuery.checkInDate);
+    const checkOut = moment(searchQuery.checkOutDate);
+    if (checkIn.isValid() && checkOut.isValid()) {
+      setErrorMessage("");
+    }
+  };
+
+  const ClearSearch = () => {
+    setSearchQuery({
+      checkInDate: "",
+      checkOutDateP: "",
+      roomType: "",
+    });
+  };
+
+  return <div>RoomSearch</div>;
+};
+
+export default RoomSearch;
