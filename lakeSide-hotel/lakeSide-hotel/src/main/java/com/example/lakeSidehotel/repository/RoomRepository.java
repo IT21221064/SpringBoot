72b30036c3643a8c8423092e61fc814d082766fd
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-02-15 17:20:03 +0530

    change sql query for available room repository

--- lakeSide-hotel/lakeSide-hotel/src/main/java/com/example/lakeSidehotel/repository/RoomRepository.java
+++ lakeSide-hotel/lakeSide-hotel/src/main/java/com/example/lakeSidehotel/repository/RoomRepository.java
@@ -13,12 +13,12 @@ public interface RoomRepository extends JpaRepository<Room,Long> {
     @Query("SELECT DISTINCT r.roomType FROM Room r")
     List<String> findDistinctRoomTypes();
 
-    @Query("SELECT r FROM Room r"+
-    "WHERE r.roomType LIKE %:roomType%"+
-    "AND r.id NOT IN ("+
-    "AND r.id NOT IN ("+
-            "SELECT br.room.id FROM BookedRoom br"+
-            "WHERE ((br.checkInDate <= :checkOutDate) AND (br.checkOutDate >= :checkInDate))"+
-    ")")
+    @Query("SELECT r FROM Room r " +
+            "WHERE r.roomType LIKE %:roomType% " +
+            "AND r.id NOT IN (" +
+            "SELECT br.room.id FROM BookedRoom br " +
+            "WHERE (br.checkInDate <= :checkOutDate AND br.checkOutDate >= :checkInDate)" +
+            ")")
     List<Room> findAvailableRoomsByDatesAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
+
 }
