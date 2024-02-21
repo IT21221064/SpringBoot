b3134e9bf0d42d7ce5b972d34fecc9c0901f67ba
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2024-02-21 23:18:48 +0530

    role model done

--- lakeSide-hotel/lakeSide-hotel/src/main/java/com/example/lakeSidehotel/model/Role.java
+++ lakeSide-hotel/lakeSide-hotel/src/main/java/com/example/lakeSidehotel/model/Role.java
@@ -1,9 +1,6 @@
 package com.example.lakeSidehotel.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,6 +18,7 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @ManyToMany(mappedBy = "roles")
     private Collection<User> users = new HashSet<>();
     public void assignRoleToUser(User user){
         user.getRoles().add(this);
